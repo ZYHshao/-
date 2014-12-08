13f72b432a40111310f0c8950a616d39e002622c
大脚a <ooashia-1@dajiaoadeMac-mini.local>
2014-12-08 14:26:21 +0800

    Initial Commit

---
+++ MyStickHero/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  MyStickHero
+//
+//  Created by 大脚a on 14-12-8.
+//  Copyright DJ_ZYH 2014年. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ MyStickHero/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MyStickHero
+//
+//  Created by 大脚a on 14-12-8.
+//  Copyright DJ_ZYH 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
